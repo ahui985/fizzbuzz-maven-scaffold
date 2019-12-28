5f3dc7775dacb646c17f34081de35602e7e23567
unknown <guohui985@163.com>
2019-12-28 12:12:18 +0800

    refactor

--- src/main/java/fizzBuzz.java
+++ src/main/java/fizzBuzz.java
@@ -1,15 +1,27 @@
 public class fizzBuzz {
 
-    public static String isFizzbuss(int i){
+    public static String isFizzbuss(int i) {
         //最小公倍数计算
-        if(i % 15 ==0)return "FizzBuzz";
-        if(i % 3 ==0) return "Fizz";
-        if(i % 5 ==0) return "Buzz";
-        return String.valueOf(i);
+        String returnValue = "";
+        if (i % 15 == 0) {
+            returnValue = "FizzBuzz";
+        }
+        if (i % 3 == 0) {
+            returnValue = "Fizz";
+        }
+        if (i % 5 == 0) {
+            returnValue = "Buzz";
+        }
+        returnValue = String.valueOf(i);
+        return returnValue;
     }
+    /**
+     * 主类 运行方法
+     * @param args
+     */
     public static void main(String[] args) {
 
-        for (int i=1; i<=100; i++) {
+        for (int i = 1; i <= 100; i++) {
             System.out.println(isFizzbuss(i));
 
         }
