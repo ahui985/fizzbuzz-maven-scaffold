1d4d8afba074b15a6ec801d1ca4b6f17a7d77f18
unknown <guohui985@163.com>
2019-12-28 11:13:46 +0800

    redtest

--- src/main/java/fizzBuzz.java
+++ src/main/java/fizzBuzz.java
@@ -1,7 +1,16 @@
 public class fizzBuzz {
+
+    public static String isFizzbuss(int i){
+        //最小公倍数计算
+        if(i % 15 ==0)return "FizzBuzz";
+        if(i % 3 ==0) return "Fizz";
+        if(i % 5 ==0) return "Buzz";
+        return String.valueOf(i);
+    }
     public static void main(String[] args) {
 
         for (int i=1; i<=100; i++) {
+            System.out.println(isFizzbuss(i));
 
         }
 
---
+++ src/test/java/redTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+//import static org.assertj.core.api.Assertions.
+
+public class redTest {
+    @Test
+    public void RainyTest(){
+        Assert.assertEquals("Oops,error!",fizzBuzz.isFizzbuss(3));
+    }
+}
